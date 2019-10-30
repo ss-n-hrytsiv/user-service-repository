23472c80482b8fce8086f0d1458a9f8f5181be4c
ss-n-hrytsiv <adidas855@ukr.net>
2019-10-31 00:28:47 +0300

    added structure of user-service

---
+++ run.py
@@ -0,0 +1,9 @@
+from user_service import app
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ user_service/__init__.py
@@ -0,0 +1,4 @@
+"""Init the user_service"""
+from flask import Flask
+
+app = Flask(__name__)
